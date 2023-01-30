c223a3552e57840c4881cbdb661627e4fe255ed3
Woxet <seillierjordan1@gmail.com>
2023-01-30 22:04:55 +0100

    Update de la fonction searchByDate

--- src/main/java/agenda/synchro/rdv/Database.java
+++ src/main/java/agenda/synchro/rdv/Database.java
@@ -11,21 +11,19 @@ public class Database {
         list.add(new RDV( 2, "Soutenance", "2023-01-28", "15:30", "UPJV"));
     }
 
-    public static RDV[] searchByDate(String date) {
+    public static ArrayList<RDV> searchByDate(String date) {
         // Initialise un tableau de RDV pour stocker les résultats de la recherche
-        RDV[] result = new RDV[list.size()];
-        int index = 0;
+        ArrayList<RDV> result = new ArrayList<>();
         // Parcours le tableau de RDV
         for (RDV rdv : list) {
             // Si la date du RDV est égale à la date recherchée
             if (rdv.getDate().equals(date)) {
                 // Ajoute le RDV au tableau de résultats
-                result[index] = rdv;
-                index++;
+                result.add(rdv);
             }
         }
         // Retourne le tableau de résultats
-        return Arrays.copyOf(result, index);
+        return result;
     }
 
 }
